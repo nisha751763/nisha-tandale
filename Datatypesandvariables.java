f1816c264e6cb2f088ca93d2f9adbf46bd5a810f
nisha tandale <ntandale167@gmail.com>
2025-07-31 08:36:16 -0600

    This is my Twelve commit

---
+++ Datatypesandvariables.java
@@ -0,0 +1,22 @@
+public class Datatypesandvariables{
+
+    public static void main(String args[]){
+        boolean b1=true;
+        char ch='A';
+        byte by=123;
+         int intData=123456;
+       
+        float f1=12345.1f;
+        System.out.println(b1);
+         System.out.println(ch);
+          System.out.println(by);
+          System.out.println(intData);
+             System.out.println(f1);
+            
+           String st="hello word";
+              System.out.println(st);
+               int result[]={50, 60, 80};
+               System.out.println(result);
+        
+    }
+}
