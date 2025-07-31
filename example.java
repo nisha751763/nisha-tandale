8d7ae4baf24afccbe492b333c1bf9297a161b1c7
nisha tandale <ntandale167@gmail.com>
2025-07-31 08:06:51 -0600

    this is my commit

---
+++ example.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class example{
+public static void main( String args[]){
+    System.out.println("args.length");
+    System.out.println("you have enter name is");
+for(int i=0;i<args.length;i++){
+System.out.println(args[i]);
+}
+
+
+
+}
+
+}
