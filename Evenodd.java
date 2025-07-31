8e0a767a9250ddb6ff8ac96824bdee4c696e8350
nisha tandale <ntandale167@gmail.com>
2025-07-31 08:18:02 -0600

    this is my seven commit

---
+++ Evenodd.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Evenodd{
+
+public static void main( String args[]){
+
+for(int i=0;i<=100;i++){
+if(i%2!=0){
+
+System.out.println( i + " is odd");
+}else{
+System.out.println( i +  "is even");
+}
+}
+}
+}
