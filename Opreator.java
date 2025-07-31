ca2b907ba2ff4e3edbdc83f9838c1335dadc29c7
nisha tandale <ntandale167@gmail.com>
2025-07-31 08:47:09 -0600

    This is my Fifteen commit

---
+++ Opreator.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Opreator{
+
+public static void main(String arg[]){
+
+int factorial=1,number=5;
+for(int i=1;i>=0;i--){
+factorial=factorial*i;
+
+}
+ System.out.println(" factorial of " + number+ "is" +factorial);
+}
+}
