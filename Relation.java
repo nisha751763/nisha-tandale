e2e49cdf4eec4897d318d3eea4c8d07ce6b3daeb
nisha tandale <ntandale167@gmail.com>
2025-08-01 11:03:31 -0600

    This is my Seventeen commit

---
+++ Relation.java
@@ -0,0 +1,12 @@
+public class Relation{
+
+public static void main(String arg[]){
+
+int factorial=1,number=5;
+for(int i=1;i>=0;i--){
+factorial=factorial*i;
+
+}
+ System.out.println(" factorial of " + number+ "is" +factorial);
+}
+}
